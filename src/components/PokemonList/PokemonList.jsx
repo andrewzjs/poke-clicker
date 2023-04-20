8e59dc4cf4e3a8452a4d419128ced08a3832d105
andrewzjs <drewsnts21@gmail.com>
2023-04-20 12:37:54 -0400

    fix table

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,15 +1,24 @@
 import { useState, useEffect } from "react";
 import * as pokemonAPI from '../../utilities/pokemonApi'
+import PokemonListItem from "../PokemonListItem/PokemonListItem";
+import './PokemonList.css'
 
 export default function PokemonList ({ pokemon }) {
-    return(
+
+    return (
         <>
-            <div>
-                Pokemon List
-            </div>
-            <div>
-                { pokemon[0]?.name }
-            </div>
+        <div class="table-container">
+          <table>
+            <thead>
+              <th> Pokemon</th>
+            </thead>
+            <tbody>
+            {pokemon.map((p, index) => (
+                <PokemonListItem pokemon={p} key={index} index={index} />
+            ))}
+            </tbody>
+          </table> 
+        </div>
         </>
     )
 }
