97569e4cd6ade19d47eb30ab593f9c735619a7d1
andrewzjs <drewsnts21@gmail.com>
2023-04-20 03:41:33 -0400

    add some animations

--- src/components/PokedexList/PokedexList.jsx
+++ src/components/PokedexList/PokedexList.jsx
@@ -9,11 +9,50 @@ export default function PokedexList({user, setUser}) {
           <table>
             <thead>
               <th style={{visibility:"hidden"}}>ssssssssss</th>
-              <th >Pokemon</th>
+              <th style={{paddingLeft: "3%"}}> Pokemon</th>
               <th style={{visibility:"hidden"}}>sssssss</th>
     
             </thead>
             <tbody>
+              <tr>
+                <td id="zapdos-sprite-table"></td>
+                <td className="pokemon-name-table">Zapdos</td>
+                <td>
+                  <button className="button-table"> Details </button>
+                  <button className="button-table"> Release </button>
+                </td>
+              </tr>
+              <tr>
+                <td id="milotic-sprite-table"></td>
+                <td>Milotic</td> <td>
+                  <button className="button-table"> Details </button>
+                  <button className="button-table"> Release </button>
+                </td>
+              </tr>
+              <tr>
+                <td id="gardevoir-sprite-table"></td>
+                <td className="pokemon-name-table">Gardevoir</td>
+                <td>
+                  <button className="button-table"> Details </button>
+                  <button className="button-table"> Release </button>
+                </td>
+              </tr>
+              <tr>
+                <td id="salamence-sprite-table"></td>
+                <td className="pokemon-name-table">Salamence</td>
+                <td>
+                  <button className="button-table"> Details </button>
+                  <button className="button-table"> Release </button>
+                </td>
+              </tr>
+              <tr>
+                <td id="blissey-sprite-table"></td>
+                <td className="pokemon-name-table">Blissey</td>
+                <td>
+                  <button className="button-table"> Details </button>
+                  <button className="button-table"> Release </button>
+                </td>
+              </tr>
               <tr>
                 <td id="zapdos-sprite-table"></td>
                 <td className="pokemon-name-table">Zapdos</td>
