5cdaaaee8e669f4f668f1e75d4b6566149b16f31
andrewzjs <drewsnts21@gmail.com>
2023-04-21 15:32:14 -0400

    edit pokedex page

--- src/pages/PokedexPage/PokedexPage.jsx
+++ src/pages/PokedexPage/PokedexPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import * as pokedexAPI from '../../utilities/pokedexApi'
-
+import './PokedexPage.css'
 
 export default function PokedexPage({}) {  
   const [pokedex, setPokedex] = useState([])
@@ -14,14 +14,31 @@ export default function PokedexPage({}) {
     getPokedex()
 }, [])
 
-    return (
-        <h1>{pokedex.map((pokemon, index) => (
-          <>
-          {pokemon.name}
-          <br />
-          {pokemon.sprite}
-          </>
-        ))}</h1>
-    )
-  
-  }
+return (
+  <div class ="pokedex-container"> 
+  <h2 style={{color:"white", paddingTop:"1%"}}>Pokedex Entries</h2>
+    <table class="pokedex-index">
+      <thead>
+        <tr>
+          <th>Sprite</th>
+          <th>Pokemon</th>
+          <th>Type</th>
+          <th>Ability</th>
+          <th>Date Caught</th>
+        </tr>
+      </thead>
+      <tbody>
+ {pokedex.map(pokemon => (
+    <tr>
+      <td><img src={pokemon.sprite}/></td>
+      <td>{pokemon.name}</td>
+      <td>{pokemon.pokemonType}</td>
+      <td>{pokemon.ability}</td>
+      <td>{pokemon.dateCaught}</td>
+    </tr>
+  ))} 
+      </tbody>
+    </table>
+  </div>
+)
+}
