0897a7b97df90a7bbf7bd15673e0d322a148f969
andrewzjs <drewsnts21@gmail.com>
2023-04-23 19:03:31 -0400

    edit gameboard mechanic and finish pokedex page

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,22 +1,68 @@
 import './GameBoard.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from 'axios';
 
-export default function GameBoard({ handleAddPokemon}) {
+export default function GameBoard({ handleAddPokemon }) {
+    const characterSprite = "https://i.imgur.com/xKT0jKW.png"
+    const [box, setBox] = useState(null)
+    const [pokemonSprite, setPokemonSprite] = useState(null)
+    const [newPokemon, setNewPokemon] = useState(null)
+    const [newPokedexEntry, setNewPokedexEntry] = useState(null)
 
     function getRndInteger() {
-        return Math.floor(Math.random() * 900 ) + 1;
+        return Math.floor(Math.random() * 150 ) + 1;
     }
 
-    async function generatePokemon() {
+    function randomBox(){
+        console.log('random box called')
+        return Math.floor(Math.random() * 23 )
+    }
+
+    // initializes div and id of 24 boxes from top to bottom in the game grid
+    const boxes = []
+    for(let i=0; i<24; i++ ) {
+        if (box === i) {
+            boxes.push(<div key={i} id={`box${i}`} className="sprite wild-pokemon"  onClick={() => handleClick()}><img src={pokemonSprite} alt="" /></div>)
+        } else {
+            boxes.push(<div key={i} id={`box${i}`} className="sprite"></div>)
+        }
+    }
+    
+    useEffect(function() {
+        async function generatePokemon() {
+            setTimeout(() => {
+                fetchRandPokemon()
+                setBox(randomBox())
+                setTimeout(() => {
+                    setBox(null)
+                    setPokemonSprite(null)
+                    setNewPokedexEntry(null)
+                    setNewPokemon(null)
+                }, 2000)
+                generatePokemon()
+            }, 2000)
+        }
+        generatePokemon()
+    }, [])
+
+    async function handleClick() {
+        setBox(null)
+        setPokemonSprite(null)
+        setNewPokedexEntry(null)
+        setNewPokemon(null)
+        handleAddPokemon(newPokemon, newPokedexEntry)
+    }
+
+    async function fetchRandPokemon() {
+        console.log('fetch pokemon called')
         try {
             const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${getRndInteger()}`)
             const name = response.data.name
             const sprite  = response.data.sprites.front_default
+            setPokemonSprite(sprite)
             const pokemonType=[]
             const ability = response.data.abilities[0].ability.name
             const moveDetails = []
-            const pokedexNum = response.data.id
             if (response.data.types.length > 1) {
                 pokemonType.push(response.data.types[0].type.name)
                 pokemonType.push(response.data.types[1].type.name)
@@ -27,7 +73,7 @@ export default function GameBoard({ handleAddPokemon}) {
             let i = 0
             let moveArray=[]
             while (i<4) {
-                var move = response.data.moves[Math.floor(Math.random()*response.data.moves.length)]
+                var move = await response.data.moves[Math.floor(Math.random()*response.data.moves.length)]
                 if(!moveArray.includes(move.move)){
                     moveArray.push(move.move)
                 }
@@ -56,11 +102,10 @@ export default function GameBoard({ handleAddPokemon}) {
                 sprite: sprite,
                 pokemonType: pokemonType,
                 ability: ability,
-                dateCaught: Date.now(),
-                pokedexNum: pokedexNum
+                dateCaught: Date.now()
             }
-            console.log(newPokedexEntry.pokedexNum)
-            handleAddPokemon(newPokemon, newPokedexEntry)
+            setNewPokemon(newPokemon)
+            setNewPokedexEntry(newPokedexEntry)
         } catch(err) {
             console.log(err)
         }
@@ -69,9 +114,10 @@ export default function GameBoard({ handleAddPokemon}) {
     return (
         <>
             <div className = "board">
-                <div id="board-sprite"></div>
-                <div id="pokemon1"  onClick={() => generatePokemon()}>
+                <div id="board-sprite">
+                    <img className="character-sprite" src={characterSprite} alt="" />
                 </div>
+            {boxes}
             </div>
         </>
     )
