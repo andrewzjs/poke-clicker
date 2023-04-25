fab76fcfb2335ff56c17ce95b9d4c19ed9dbde83
andrewzjs <drewsnts21@gmail.com>
2023-04-25 11:38:31 -0400

    stuff

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -5,9 +5,15 @@ import * as pokemonStatsAPI from '../../utilities/pokemonStatsApi'
 import { useState, useEffect } from "react";
 import GameBoard from '../GameBoard/GameBoard'
 import PokemonList from '../PokemonList/PokemonList'
+import img1 from '../../images/env-1.png'
+import img2 from '../../images/env-2.png'
+import img3 from '../../images/env-3.png'
+import img4 from '../../images/env-4.png'
+
 
 export default function Game({ handleAddNewPokemon }) {
     const [pokemon, setPokemon] = useState([])
+    const [background, setBackground] = useState(img1)
 
     async function handleAddNewPokemon(newPokemon, newPokedexEntry, newPokemonStats){
         const pokemonFromDatabase = await pokemonAPI.getAll();
@@ -28,6 +34,22 @@ export default function Game({ handleAddNewPokemon }) {
         console.log('remove pokemon', pokeId)
     }
 
+    async function handleChangeBackground1(){
+        setBackground(img1)
+    }
+
+    async function handleChangeBackground2(){
+        setBackground(img2)
+    }
+
+    async function handleChangeBackground3(){
+        setBackground(img3)
+    }
+
+    async function handleChangeBackground4(){
+        setBackground(img4)
+    }
+
     useEffect(function() {
         async function getPokemon() {
             const pokemon = await pokemonAPI.getAll()
@@ -40,12 +62,18 @@ export default function Game({ handleAddNewPokemon }) {
         <div className="home-area">
             <div className="gameboard-area">
                 <div className="env-toggle">
-                    <button id="env1" >Grassy Terrain</button>
-                    <button id="env2" >Stratosphere</button>
-                    <button id="env3" >Distortion World</button>
-                    <button id="env4" >Sapphire Sea</button>
+                    <button id="env1" onClick={() => handleChangeBackground1()}>Grassy Terrain</button>
+                    <button id="env2" onClick={() => handleChangeBackground2()}>Stratosphere</button>
+                    <button id="env3" onClick={() => handleChangeBackground3()}>Distortion World</button>
+                    <button id="env4" onClick={() => handleChangeBackground4()}>Sapphire Sea</button>
                 </div>
-                <GameBoard handleAddPokemon={handleAddNewPokemon}/>
+                <GameBoard 
+                background={ background } 
+                handleChangeBackground1={handleChangeBackground1}
+                handleChangeBackground2={handleChangeBackground2}
+                handleChangeBackground3={handleChangeBackground3}
+                handleChangeBackground4={handleChangeBackground4} 
+                handleAddPokemon={handleAddNewPokemon}/>
             </div>
             <div className="pokemonlist-area">
                 <PokemonList pokemon={ pokemon } setPokemon={ setPokemon } handleRemovePokemon={handleRemovePokemon} />
